5a96c71a9fb5607019eaa52aca76c0c3a50cf951
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-05-09 20:38:03 +0700

    adding componen

---
+++ app.js
@@ -0,0 +1,8 @@
+const bioData = require("./modules/bioData");
+
+console.log(`
+Nama : ${bioData.nama}
+Tempat Lahir : ${bioData.tempatLahir}
+Tanggal Lahir : ${bioData.tanggalLahir}
+Alamat : ${bioData.alamat}
+`)
---
+++ modules/bioData.js
@@ -0,0 +1,8 @@
+const bioData = {
+    'nama': 'ikhsan',
+    'tempatLahir': 'cianjur',
+    'tanggalLahir': '16 Juli 2003',
+    'alamat': 'jl desa bunijaya, pagelaran'
+}
+
+module.exports = bioData
